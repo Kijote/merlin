29aefe45c98637a8dfcecd2bc724caa36c51a2e6
Andrés Morales <andres.morales@sitrack.com>
2021-08-08 20:21:39 -0300

    Se agregan las migraciones para DB

---
+++ database/migrations/2021_08_06_205250_create_satellite_data_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSatelliteDataTable extends Migration
+{
+/*
+        CREATE TABLE satellite_data (
+            data_id int NOT NULL AUTO_INCREMENT, 
+            name varchar(20), 
+            request_time timestamp, 
+            distance float, 
+            message varchar(512),
+            PRIMARY KEY (data_id)
+        );
+*/
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('satellite_data', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->primary('id');
+            $table->string('name');
+            $table->timestamp('request_time');
+            $table->float('distance');
+            $table->string('message');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('satellite_data');
+    }
+}
