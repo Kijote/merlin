a339a1bf2de6c1131bfb170918c260deffd43d18
Andres Morales <yo@kijote.com.ar>
2021-08-09 19:01:36 -0300

    Agrego el MessageHelper

---
+++ app/MessageHelper.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+use \Exception;
+
+/**
+ * Helper de Mensajes para challenge de Mercado Libre.
+ */
+class MessageHelper{
+    
+    static function getMessage($d1, $d2, $d3) {
+        // Armo un stack para los mensajes
+        $stack = [];
+
+        // Array de mensajes
+        $messages = [$d1, $d2, $d3];
+
+        // Obtengo la menor cantidad de índices de los arrays pasados
+        $count = min(array_map('count', $messages));
+
+        // Elimino el posible "delay" entre los mensajes
+        $messages = array_map(function ($item) use ($count) {
+            return array_slice($item, count($item) - $count);
+        }, $messages);
+
+        // Itero por cada ítem de los arrays
+        for($i = 0; $i < $count; $i++){
+            $stack[] = array_reduce([$messages[0][$i], $messages[1][$i], $messages[2][$i]], function($carry, $item){ return !empty($carry) ? $carry : $item; });
+        }
+
+        // uno el stack con espacios y retorno el mensaje
+        return implode(' ', $stack);
+    }
+}
